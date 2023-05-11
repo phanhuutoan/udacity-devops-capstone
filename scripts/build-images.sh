d1e73dd748de30bb64cea7b0d01f83ede19036fd
phanhuutoan.dev <phanhuutoan.dev@outlook.com>
2023-05-11 08:11:39 +0700

    setup local k8s

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,13 +1,14 @@
 #/bin/bash
 
 BUILD=$1
+TAG=$2
 
 if [ $BUILD = 'be' ]
 then
-    docker build -f Dockerfile.be --build-arg BUILD_CONTEXT=todo-be . -t superknife0512/todo-be
+    docker build -f Dockerfile.be --build-arg BUILD_CONTEXT=todo-be . -t superknife0512/todo-be:$TAG
 elif [ $BUILD = 'fe' ]
 then 
-    docker build -f Dockerfile.fe --build-arg BUILD_CONTEXT=todo-fe . -t superknife0512/todo-fe
+    docker build -f Dockerfile.fe --build-arg BUILD_CONTEXT=todo-fe . -t superknife0512/todo-fe:$TAG
 else 
     echo "Cannot process please add 'fe' or 'be'"
     exit 1
---
+++ scripts/push-images.sh
@@ -0,0 +1,3 @@
+
+docker push superknife0512/todo-fe
+docker push superknife0512/todo-be
